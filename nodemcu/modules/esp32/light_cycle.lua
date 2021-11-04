82eb4018e927aba035188aab3c700621c84ab528
eliasriise <eliasriise@outlook.com>
2021-11-04 12:44:33 +0100

    update light cycle

--- nodemcu/modules/esp32/light_cycle.lua
+++ nodemcu/modules/esp32/light_cycle.lua
@@ -7,34 +7,35 @@ local json_file_to_dict = require('json').json_file_to_dict -- Magic
 timer = tmr.create() -- Create timer
 
 config = json_file_to_dict('config.json')
-pin = config.pins.light_signal -- Pin used for signaling the relay to be on or off -- Use config file for values
 
+pin = config.pins.light_signal -- Pin used for signaling the relay to be on or off -- Use config file for values
 gpio.config(
     {
         gpio=pin, 
         dir=gpio.OUT
     }
 )
-
 on =  config.light.on
 off = 1 - on
 light_status = -1
 
 -- Time interval when the light should be on
-on_from = config.light_conf.light_on_from
-on_to = config.light_conf.light_on_to
+on_from = config.light.light_on_from
+on_to = config.light.light_on_to
 
-timer:alarm(30000, tmr.ALARM_AUTO, function() -- Check clock every minute
+timer:alarm(20000, tmr.ALARM_AUTO, function() -- Check clock every minute
     time = get_time_now()
     tmp_t = tonumber(string.sub(time, 1, 2)) -- Look only at hours
 
     if on_from <= tmp_t and tmp_t < on_to then -- time is inside 'on' interval
         if light_status ~= on then
+            print("Light on at", on_from, ", now it is", tostring(tmp_t))
             light_status = on
             gpio.write(pin, on)
         end
     else -- Time is outside 'on' interval
         if light_status ~= off then 
+            print("Light off at", on_to, ", now it is", tostring(tmp_t))
             light_status = off
             gpio.write(pin, off)
         end
