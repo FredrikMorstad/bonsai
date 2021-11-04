f96612f4caaf16e0467560de165912607e20378a
Fredrik Mørstad <Fredrik.Morstad@hotmail.com>
2021-11-04 13:47:32 +0100

    :tada: init frontend

---
+++ frontend/src/components/molecules/Navbar/Navbar.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Box } from '@chakra-ui/react';
+
+const Navbar = () => {
+  return(
+    <Box h="5vh" w="100%" bg="blue"/>
+  )
+}
+
+export default Navbar;
---
+++ frontend/src/components/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Box } from '@chakra-ui/react';
+
+const HomePage = () => {
+  return(
+    <Box>
+      test
+    </Box>
+  )
+}
+
+export default HomePage;
