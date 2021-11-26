c5d3049593c3c1f59977125c99eaab7aeb98572c
Fredrik Mørstad <Fredrik.Morstad@hotmail.com>
2021-11-26 17:17:40 +0100

    :sparkles: add profile page template

---
+++ frontend/src/components/pages/Profile/Profile.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProfilePage: React.FC = () => {
+  return (
+    <div>
+      This page should require a user to be logged in!
+    </div>
+  )
+}
+
+export default ProfilePage
