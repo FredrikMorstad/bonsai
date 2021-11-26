057d2bd0fee95b00d0b9fce99e5f4ea5b830c5dc
Fredrik Mørstad <Fredrik.Morstad@hotmail.com>
2021-11-26 17:17:40 +0100

    :sparkles: add profile page template

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import HomePage from "./components/pages/Home";
 import Navbar from "./components/molecules/Navbar/Navbar";
+import { AuthenticateContext } from "context/authProvider";
+import PrivateRoute from "routes/privateRoute";
+import { HomePage, LoginPage, ProfilePage } from "components/pages/index";
 
 export const App = () => {
+  const { authenticated } = useContext(AuthenticateContext);
+  console.log(`auth: ${authenticated}`);
   return (
     <Router>
       <Navbar />
       <Switch>
+        <Route path="/login" component={LoginPage} />
+        <PrivateRoute path="/profile" component={ProfilePage} />
         <Route path="/" component={HomePage} />
       </Switch>
+      <p>login: {`${authenticated}`}</p>
     </Router>
   );
 };
