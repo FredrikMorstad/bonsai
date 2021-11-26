875d1ae865c29c5645d41d2a98f3f6499185f59d
Fredrik Mørstad <Fredrik.Morstad@hotmail.com>
2021-11-26 14:56:22 +0100

    :sparkles: add login/logout buttons

--- frontend/src/components/molecules/Navbar/Navbar.tsx
+++ frontend/src/components/molecules/Navbar/Navbar.tsx
@@ -1,10 +1,33 @@
-import React from 'react';
-import { Box } from '@chakra-ui/react';
+import React, { useContext } from "react";
+import { Box, Button, Center, Flex } from "@chakra-ui/react";
+import { useHistory } from "react-router-dom";
+import { AuthenticateContext } from "context/authProvider";
+import { clearTokens } from "api/token";
 
+// just a temporary navbar containing login button
 const Navbar = () => {
-  return(
-    <Box h="5vh" w="100%" bg="blue"/>
-  )
-}
+  const { authenticated, setAuthenticated } = useContext(AuthenticateContext);
+  const history = useHistory();
+
+  const moveToLogin = () => history.push("/login");
+
+  const logout = () => {
+    clearTokens();
+    setAuthenticated(false);
+    history.push("/home");
+  };
+
+  return (
+    <Flex h="7.5vh" w="100%" bg="blue" justifyContent="flex-end">
+      <Center mr=".75rem">
+        {authenticated ? (
+          <Button onClick={logout}> Logout </Button>
+        ) : (
+          <Button onClick={moveToLogin}> Login </Button>
+        )}
+      </Center>
+    </Flex>
+  );
+};
 
 export default Navbar;
