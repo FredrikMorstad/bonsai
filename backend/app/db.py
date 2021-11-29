b713b34d0a0ff7b7b213800f0e89dcbc54241457
Fredrik Mørstad <Fredrik.Morstad@hotmail.com>
2021-11-29 22:58:20 +0100

    :adhesive_bandage: only create db folder and file in local environment

--- backend/app/db.py
+++ backend/app/db.py
@@ -15,10 +15,12 @@ for e in env:
 
 config = env_config[env_variables]
 
-if not os.path.exists(config.DB_FOLDER):
-    os.mkdir(config.DB_FOLDER)
-if not os.path.exists(config.DB_URI):
-    open(config.DB_URI, 'w')
+# prod and dev db mounted in dockerfile
+if env_variables == 'local':
+    if not os.path.exists(config.DB_FOLDER):
+        os.mkdir(config.DB_FOLDER)
+    if not os.path.exists(config.DB_URI):
+        open(config.DB_URI, 'w')
     
 engine = create_engine(f'sqlite:///{config.DB_URI}', connect_args={'check_same_thread': False})
 session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
